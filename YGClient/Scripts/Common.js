01d04148ab2b025c8651f6ccf2f7d0de5a9287ca
Runesis <jjadoll@hotmail.com>
2017-10-17 19:03:09 +0900

    work login form

---
+++ YGClient/Scripts/Common.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+$().ready(function () {
+});
+
+function showScreen(screenId) {
+    var activeScreen = $("#Client .Screen.Active")[0];
+       // screen = $("#" + screenId)[0];
+    if (activeScreen) {
+        $("#Client .Screen.Active").removeClass("Active");
+    }
+    $("#" + screenId).addClass('Active');
+    //screen.addClass(".screen.active");
+}
---
+++ YGClient/Scripts/Login.js
@@ -0,0 +1,40 @@
+﻿"use strict";
+
+var formType = 'login';
+
+$().ready(function () {
+    $('div.background').fadeIn(3000, function () {
+        $('div.form').fadeIn(1800).queue(function () {
+            $('div.form').css('display', 'block');
+            console.log("login form load complete");
+        });
+    });
+
+    $('.message a').click(function () {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow", function () {
+            if (formType == 'login') {
+                $('#logEmail').val('');
+                $('#logPassword').val('');
+                formType = 'reg'
+            }
+            else {
+                $('#regEmail').val('');
+                $('#regPassword').val('');
+                $('#privatenum').val('');
+                formType = 'login';
+            }
+        });
+    });
+
+    $('form.login-form button').click(function () {
+        Login();
+    });
+});
+
+function Login()
+{
+    showScreen('WorldMap');
+}
+
+function Register()
+{ }
